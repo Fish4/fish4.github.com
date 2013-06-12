8c6648b2a37ac436c9fce81f86d20f06d7906848
Eric Liao <rcliao01@gmail.com>
2013-06-12 16:17:31 -0700

    implement basic carousel

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,3 +1,15 @@
 'use strict';
 
 /* Controllers */
+
+function CarouselCtrl($scope) {
+	$scope.myInterval = 5000;
+	$scope.indexSlides = [
+		{image: 'AW_gallery/Screenshot.png'},
+		{image: 'AW_gallery/Screenshot2.png'},
+		{image: 'AW_gallery/Screenshot3.png'},
+		{image: 'AW_gallery/Screenshot4.png'},
+		{image: 'AW_gallery/Screenshot5.png'},
+		{image: 'AW_gallery/Screenshot6.png'}
+	];
+}
